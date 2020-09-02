6f886120d828c41dc21e128963a5e6b897d3c3ec
unknown <kristofermani@live.com>
2020-09-02 13:08:04 +0000

    awawa

---
+++ Verkefni1/Scripts/PlayerCollision.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class PlayerCollision : MonoBehaviour{
+
+    public PlayerMovement movement; //tengir við okkar hreyfi skriftu
+
+    // þessi skrifa keyrir þegar við snertum annan hlut og nær í info um það sem við snertum
+    void OnCollisionEnter(Collision collisionInfo)
+    {
+        // checka hvort við höfum snert hluti með tag obstacle
+        if (collisionInfo.collider.tag == "Obstacle")
+        {
+         movement.enabled = false; //stopa spillara hreyfingar
+         FindObjectOfType<GameManager>().EndGame();
+        }
+    }
+
+}
---
+++ Verkefni1/Scripts/PlayerMovement.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+
+    public Rigidbody rb; //breytir nafn svo léttara að skrifa
+
+    public float forwardForce = 2000f;
+    public float sidewaysForce = 500f;
+    // Update is called once per frame
+    void FixedUpdate() // FixedUpdate vegna unity höndlar physics betra með það
+    {
+        rb.AddForce(0,0,forwardForce * Time.deltaTime); // Time.deltaTime notað til að laga fps vandamál milli tölvur
+
+        if ( Input.GetKey("d"))
+        {
+            rb.AddForce(sidewaysForce * Time.deltaTime,0,0, ForceMode.VelocityChange);
+        }
+        if ( Input.GetKey("a"))
+        {
+            rb.AddForce(-sidewaysForce * Time.deltaTime,0,0, ForceMode.VelocityChange);
+        }
+
+        if (rb.position.y < -1f)
+        {
+            FindObjectOfType<GameManager>().EndGame();
+        }
+    }
+}
